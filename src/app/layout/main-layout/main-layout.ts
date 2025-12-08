7ba48a19ba29c954b6f736c572b6e3ead657d20a
MikeRbl <maicoalf77@gmail.com>
2025-12-08 00:25:03 -0600

    Implenatacion de vistas segun rol

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
-export class MainLayout {
+export class MainLayout implements OnInit {
+  userRole: string = "";
 
+  constructor(private router: Router){}
+
+  ngOnInit() {
+    this.userRole = localStorage.getItem('rol') || '';
+  }
+
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
